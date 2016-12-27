73091755befdf3983b14be4c3c4554680b61668e
dam <240242398@qq.com>
2016-12-28 01:16:43 +0800

    add TodoApp

---
+++ src/Component/TodoApp.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+class TodoApp extends React.Component {
+    render() {
+        return <h1>Hello app</h1>;
+    }
+}
+
+export default TodoApp;
---
+++ src/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by dam on 2016/12/28.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoApp from './Component/TodoApp'
+
+ReactDOM.render(
+    <TodoApp />,
+    document.getElementById('app')
+);
