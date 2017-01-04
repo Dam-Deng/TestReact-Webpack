08f1a4be27792b41be4f591328a2babf61f256bd
dam <240242398@qq.com>
2017-01-05 00:30:54 +0800

    update css

--- src/Component/TodoApp.js
+++ src/Component/TodoApp.js
@@ -7,7 +7,7 @@ import TodoActions from "../Actions/TodoActions";
 class TodoApp extends React.Component {
     render() {
         const {contents} = this.props;
-        return <div>
+        return <div className="todoApp">
             <InputField onInputSubmit={this.props.inputSubmit}/>
             <ShowContent
                 contents={contents} />
